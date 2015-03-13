0c1c641662f310a51ce6e9b8c00ac58af7373191
oflannabhra <oflannabhra@gmail.com>
2015-03-13 19:39:35 -0400

    inject Repositories into Controller

--- app/Http/Controllers/TumbletController.php
+++ app/Http/Controllers/TumbletController.php
@@ -1,29 +1,46 @@
 <?php namespace Tumblet\Http\Controllers;
 
+use Illuminate\Support\Facades\View;
 use Tumblet\Http\Requests;
 use Tumblet\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
+use Tumblet\Tumblet\TumbletRepository;
+use Tumblet\Tumblet\TumbletPostRepository;
 
 class TumbletController extends Controller {
+
+	private $tumbletRepository;
+	private $tumbletPostRepository;
+
+	public function __construct (TumbletRepository $tumbletRepository, TumbletPostRepository $tumbletPostRepository)
+	{
+	    $this->tumbletRepository = $tumbletRepository;
+		$this->tumbletPostRepository = $tumbletPostRepository;
+	}
 
 	/**
 	 * Display a listing of the resource.
 	 *
 	 * @return Response
 	 */
-	public function show($tumblrName)
+	public function show($tumblrName, $pageNum)
 	{
-		dd($tumblrName);
+		$tumblet = $this->tumbletRepository->getByName($tumblrName);
+		$posts = $this->tumbletPostRepository->getPostsForTumbletAndPage($tumblet, $pageNum);
+
+		return View::make('tumblet.show')
+			->with('tumblet', $tumblet)
+			->with('posts', $posts);
 	}
 	
 	public function storeAndRedirect ()
 	{
 		$tumblrname = Input::get('tumblrname');
 
-		return Redirect::to("{$tumblrname}/");
+		return Redirect::to("{$tumblrname}/{$pageNum}");
 	}
 	
 
