8a8315c793fd34831b3334d62adcd5adf32e6c82
oflannabhra <oflannabhra@gmail.com>
2015-03-13 19:40:01 -0400

    new view to show a tumblet & posts

---
+++ resources/views/tumblet/show.blade.php
@@ -0,0 +1,17 @@
+
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">{{ $tumblet->title }}</div>
+                       {{ var_dump($posts) }}
+                    <div class="panel-body">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
