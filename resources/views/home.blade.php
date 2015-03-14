93d934c0dde3309896c7c0dbca1027f26be4a9b9
oflannabhra <oflannabhra@gmail.com>
2015-03-14 13:03:58 -0400

    rework form layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,25 @@
 @extends('app')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">tumblet</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">tumblet</div>
 
-				<div class="panel-body">
-					<form method="post" action="tumblet/create">
-						Type in a Tumblr blog name to see its posts!
-						<br>
-						<input type="text" class="input" name="tumblrname">
-						<input class="btn" type="submit">
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                    <div class="panel-body">
+                        <form class="form-inline" method="post" action="tumblet/create">
+                            <p class="instructions">
+                                Type in a Tumblr blog name to see its posts!
+                            </p>
+                            <div class="form-group">
+                                <input type="text" class="form-control text-right" name="tumblrname"> .tumblr.com
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
