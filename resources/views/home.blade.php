ec8dc7a3df4caff81bc34810557a628dbd561c32
oflannabhra <oflannabhra@gmail.com>
2015-03-14 17:21:15 -0400

    clean up title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">tumblet</div>
+                    <div class="panel-heading"><h3 class="panel-title">tumblet</h3></div>
 
                     <div class="panel-body">
                         <form class="form-inline" method="post" action="tumblet/create">
